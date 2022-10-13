380aaa2c4fc7d3c51d2cdfdee35208882c0b6029
lucaberissogonzalez <lucaberisso1@gmail.com>
2022-10-13 10:34:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombre;
+            Console.WriteLine("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Buenos dias " + nombre + " saludos");
+            Console.ReadKey();
+        }
+    }
+}
